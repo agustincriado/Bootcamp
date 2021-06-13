139d22c9898ee4d4d644716e4fa507bf966f3c58
Agustin <agustin.criado@outlook.com>
2021-06-13 14:31:55 +0200

    idk git mb

---
+++ courseinfo/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [contador, setContador] = useState(0)
+
+  const handleAugment = () => {
+    return setContador(contador + 1)
+  }
+
+  const handleDecrease = () => {
+    return contador > 0 ? setContador(contador - 1) : 'disabled = true'
+  }
+
+  const handleReset = () => {
+    return setContador(0)
+  }
+
+  const isEven = (contador % 2) === 0
+  const message = isEven ? <h1>Even</h1> : <h4>Odd</h4>
+  return (
+    <div>
+      <h1>{contador}</h1>
+      <button onClick={handleAugment}>Aumentar</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrease}>Decrementar</button>
+      <br/>
+      {message}
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
