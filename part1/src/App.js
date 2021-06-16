41ee5924a39631c40b9b3bec4780353ceb5a83b7
Agustin <agustin.criado@outlook.com>
2021-06-16 18:05:10 +0200

    part1 modified, img and Avatar fixed

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,10 +5,9 @@ const Mensaje = (props) => {
   return <h1 style={{ color: props.color}}>Saludos {props.name}!</h1>
 }
 
-const Avatar = (src , size) => {
-  <img src={src} style={{size: size}} alt="Avatar" />
+const Avatar = ({src, id}) => {
+  return <img src={src} id={id} alt='Imagen de nigator' />
 }
-
 
 function App() {
   const message = <h1>"Hola mundo!"</h1>;
@@ -19,7 +18,8 @@ function App() {
       <img src={logo} className="App-logo" alt="logo" />
       <br/>
       <Mensaje color="red" name="Agustin"/>
-      <Avatar src={props.src}/>
+      <br/>
+      <Avatar src='https://i.pinimg.com/originals/cb/fa/1c/cbfa1c6c8708fda0e3fb41875bcbdd7e.jpg' id="Avatar"/>
     </div>
   );
 }
