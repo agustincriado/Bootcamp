700ea74551580c1bad84d1fe1492fadb29c0f940
Agustin <agustin.criado@outlook.com>
2021-06-25 21:31:36 +0200

    added new conditional render

--- part4/src/App.js
+++ part4/src/App.js
@@ -4,23 +4,45 @@ import { useState } from 'react';
 
 export default function App(props) {
   const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState();
-
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+ 
   const handleChange = (event) => {
     setNewNote(event.target.value)
   }
 
   const handleClick = () => {
-    console.log(newNote);
+    const NewtoAdd = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+    setNotes([...notes, NewtoAdd]);
+    setNewNote("")
   }
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick = { handleShowAll }>{showAll ? 'show only important' : 'show all'}</button>
+      <h3>{showAll ? 'This are all notes' : 'This are only important ones'}</h3>
       <ol>
-        {notes.map(note => <Notitas key={note.id} content={note.content} date={note.date} />)}      
+        {
+        notes
+        .filter(note => {
+          if (showAll === true) return note;
+          return note.important === true
+          })
+        .map(note => <Notitas key={note.id} {...note} />
+        )}      
       </ol>
       <div>
-        <input type='text' onChange={handleChange}/>
+        <input type='text' onChange={handleChange} value={newNote}/>
         <input type='submit' value='Submit' onClick={handleClick}/>
       </div>
     </div>
--- part4/src/Notes.js
+++ part4/src/Notes.js
@@ -1,6 +1,6 @@
-export const Notitas = ({id, content, date}) => {
-
-    return  <li key={id}>
+export const Notitas = (props) => {
+    const {content, date} = props;
+    return  <li>
          <p>{content}</p>
          <small>{date}</small>
      </li>
